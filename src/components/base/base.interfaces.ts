b0f133eb21010135b9648d68ccbcd0a70763c099
Abdelrahman Ashraf <abdul.ashref333@gmail.com>
2023-01-16 10:57:19 +0200

    add base compoenent

---
+++ src/components/base/base.interfaces.ts
@@ -0,0 +1,10 @@
+export interface IWriter<T> {
+  create(item: Omit<T, 'id' | 'created_at'>): Promise<T | null>;
+  createMany(item: Omit<T, 'id' | 'created_at'>[]): Promise<T[] | null>;
+  update(con: Partial<T>, item: Partial<T>): Promise<T | null>;
+  delete(con: Partial<T>): Promise<boolean>;
+}
+export interface IReader<T> {
+  findAll(item: Partial<T>, select?: string[]): Promise<T[]>;
+  findOne(id: string | Partial<T>, select?: string[]): Promise<T | null>;
+}
---
+++ src/components/base/base.model.ts
@@ -0,0 +1,54 @@
+import { IWriter, IReader } from './base.interfaces';
+import Common from '../../utils/common';
+
+type IBaseRepository<T> = IReader<T> & IWriter<T>;
+
+export abstract class BaseModelRepository<T> implements IBaseRepository<T> {
+  constructor(public readonly tableName: string) {}
+
+  async findAll(item?: Partial<T>, select?: string[]): Promise<T[]> {
+    const result = (await Common.dbFetch(this.tableName, item, select)) as T[];
+    return result;
+  }
+
+  async findOne(item: Partial<T>, select?: string[]): Promise<T | null> {
+    const result = await Common.dbFetch(this.tableName, item, select);
+    if (result?.length) {
+      return result[0] as T;
+    } else {
+      return null;
+    }
+  }
+
+  async create(item: Omit<T, 'id' | 'created_at'>): Promise<T | null> {
+    const result = await Common.dbInsert(this.tableName, item);
+    if (result?.length) {
+      return result[0] as T;
+    }
+    return null;
+  }
+
+  async createMany(items: Omit<T, 'id' | 'created_at'>[]): Promise<T[] | null> {
+    const result = await Common.dbInsertMany(this.tableName, items);
+    if (result && result.length) {
+      return result;
+    }
+    return null;
+  }
+
+  async update(conditions: Partial<T>, item: Partial<T>): Promise<T | null> {
+    const result = await Common.dbUpdate(this.tableName, conditions, item);
+    if (result) {
+      return result[0];
+    }
+    return null;
+  }
+
+  async delete(condition: Partial<T>): Promise<boolean> {
+    const result = await Common.dbDeletion(this.tableName, condition);
+    if (result) {
+      return result;
+    }
+    return false;
+  }
+}
