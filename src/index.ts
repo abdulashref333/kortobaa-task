74d79e29d21bebc75b1a8bd3d0b014e4bcb7f87c
Abdelrahman Ashraf <abdul.ashref333@gmail.com>
2023-01-16 10:56:55 +0200

    add initial files

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import app from './app';
+import { checkingEnvVariables } from '../config/checking-env-variables';
+import Logger from './middlewares/logger';
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  checkingEnvVariables();
+  // await startDbConnection();
+
+  app.listen(port, () => Logger.info('Listening on port 3000!'));
+};
+
+start();
